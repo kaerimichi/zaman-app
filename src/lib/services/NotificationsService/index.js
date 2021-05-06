b463d68867ea57c3652f03947744eae22b541ab8
Gabriel Rimes <jivagorimes@gmail.com>
2021-05-05 22:46:05 -0300

    Add notification to the end of the day considering the hour balance (for the current month)

--- src/lib/services/NotificationsService/index.js
+++ src/lib/services/NotificationsService/index.js
@@ -2,12 +2,14 @@ import PushNotification from 'react-native-push-notification'
 
 import FirstIntervalNotificationRule from './rules/FirstIntervalNotificationRule'
 import EndOfDayNotificationRule from './rules/EndOfDayNotificationRule'
+import EndOfDayWithBalanceNotificationRule from './rules/EndOfDayWithBalanceNotificationRule'
 
 export default class NotificationsService {
   constructor (punches, statistics) {
     this.rules = [
       new FirstIntervalNotificationRule(punches, statistics),
-      new EndOfDayNotificationRule(punches, statistics)
+      new EndOfDayNotificationRule(punches, statistics),
+      new EndOfDayWithBalanceNotificationRule(punches, statistics)
     ]
 
     PushNotification.configure({
@@ -21,6 +23,7 @@ export default class NotificationsService {
     this.cancelAll()
     this.rules.forEach(notification => {
       if (notification.shouldSchedule()) {
+        console.log(`scheduling ${notification.getConfig().title}`)
         PushNotification.localNotificationSchedule(
           notification.getConfig()
         )
---
+++ src/lib/services/NotificationsService/rules/EndOfDayWithBalanceNotificationRule.js
@@ -0,0 +1,34 @@
+import BaseNotificationRule from './BaseNotificationRule'
+import pallete from 'Zaman/src/misc/pallete'
+
+export default class EndOfDayWithBalanceNotificationRule extends BaseNotificationRule {
+  shouldSchedule () {
+    const { asMinutes: remAsMinutes } = this.statistics.dayBalance.remaining
+    const { asMinutes: extraAsMinutes } = this.statistics.monthBalance.extra
+    const balanceIsNotNeutral = (remAsMinutes - extraAsMinutes) !== 0
+
+    return this.punches.length % 2 !== 0 && !balanceIsNotNeutral && this.isWeekDay()
+  }
+
+  shouldCancel () {
+    return this.punches.length % 2 === 0
+  }
+
+  getConfig () {
+    const { remaining } = this.statistics.dayBalance
+    const remainingAsMinutes = remaining.asMinutes
+    const { asMinutes: extraAsMinutes } = this.statistics.monthBalance.extra
+    const lastPunch = this.punches[this.punches.length - 1]
+    const scheduleTo = this.getMomentPlusMinutes(
+      remainingAsMinutes - extraAsMinutes,
+      this.getMoment(lastPunch, 'HH:mm')
+    )
+
+    return {
+      title: 'Fim do Expediente (com saldo)',
+      message: 'Considerando seu saldo de horas, você já trabalhou todas as horas necessárias hoje.',
+      date: scheduleTo.toDate(),
+      color: pallete.primary
+    }
+  }
+}
